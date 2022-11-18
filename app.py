5b54a61caf9f6222356a54d29191c839660e8808
Ekin VARLI <ekinnos@tutanota.com>
2022-11-19 02:14:29 +0300

    .

--- app.py
+++ app.py
@@ -52,15 +52,31 @@ from werkzeug.utils import secure_filename
 # additional modules
 import sqlite3
 import markdown
-import sass
 import os
 import time
 
 
 # settings
+UPLOAD_FOLDER = './static/uploads'
+ALLOWED_EXTENSIONS = {
+    'mp4',
+    'ogg',
+    'png',
+    'jpg',
+    'jpeg',
+    'gif'
+}
+
 app = Flask(__name__, template_folder='public')
 app.static_folder = 'static'
 app.secret_key = '1f033201deef050a886782b5'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+# functions
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 # routes
@@ -69,6 +85,67 @@ def index():
     return render_template('home.html')
 
 
+@app.route('/newcontent', methods=['GET', 'POST'])
+def new_content():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+    namesurname = request.args.get('namesurname')
+    content_title = request.args.get('title')
+    region = request.args.get('region')
+    return render_template('newcontent.html', namesurname=namesurname, title=content_title, region=region)
+    # return '''
+    # <!doctype html>
+    # <title>Upload new File</title>
+    # <h1>Upload new File</h1>
+    # <form method=post enctype=multipart/form-data>
+    #   <input type=file name=file>
+    #   <input type=submit value=Upload>
+    # </form>
+    # '''
+
+
+@app.route('/addcontent', methods=['POST', 'GET'])
+def add_content():
+    # database
+    connection = sqlite3.connect('./database/contents.db')
+    cursor = connection.cursor()
+
+    cursor.execute(
+        'CREATE TABLE IF NOT EXISTS contents(namesurname TEXT, title TEXT, region TEXT, description TEXT, filename TEXT)')
+
+
+    # datas
+    namesurname = request.args.get('namesurname')
+    content_title = request.args.get('title')
+    region = request.args.get('region')
+    description = request.args.get('description')
+    filename = request.args.get('file')
+
+
+    # insert datas
+    cursor.execute(
+        f'INSERT INTO contents VALUES("{namesurname}","{content_title}","{region}","{description}","{filename}")')
+
+    if True:
+        connection.commit()
+        connection.close()
+
+    return 'Content added.'
+
+
 # run server
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
